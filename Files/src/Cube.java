3d3d2b47abb807270e105d0203e7792249a234d4
KaziFariad <53252317+KaziFariad@users.noreply.github.com>
2022-01-17 19:24:06 +0530

    Update Cube.java

--- Files/src/Cube.java
+++ Files/src/Cube.java
@@ -6,9 +6,9 @@ public class Cube {
     }
 
     public static void main(String[] args) {
-        Cube oSheaJackson = new Cube(4);
-        System.out.println(oSheaJackson.volume());
-        System.out.println(oSheaJackson);
+        Cube cube = new Cube(4);
+        System.out.println(cube.volume());
+        System.out.println(cube);
 
         System.out.println();
 
